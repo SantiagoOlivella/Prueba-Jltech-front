46aa11826cb0cb6f285ddc8503c504d4955a6ff8
SantiagoOlivella <90742961+SantiagoOlivella@users.noreply.github.com>
2022-02-05 07:51:00 -0500

    Start Proyect
    Start proyect

---
+++ src/app/App.jsx
@@ -0,0 +1,30 @@
+import "./App.scss";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { NavBar } from "../components/navBar/NavBar";
+import { ModuleClient } from "../components/module/moduleClient/ModuleClient";
+import { ModuleProduct } from "../components/module/moduleProduct/ModuleProduct";
+import { ModuleShopping } from "../components/module/moduleShopping/ModuleShopping";
+import { ModuleUser } from "../components/module/moduleUser/ModuleUser";
+import { Login } from "../components/login/Login";
+
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" exact element={<Login />} />
+        <Route path="/client" exact element={<ModuleClient />} />
+        <Route path="/product" exact element={<ModuleProduct />} />
+        <Route path="/shopping" exact element={<ModuleShopping />} />
+        <Route path="/user" exact element={<ModuleUser />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
